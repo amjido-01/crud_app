d072bfea41c6f3899a7db876ce03ad8fa59ef74d
alajido <96017433+Alajido@users.noreply.github.com>
2022-12-09 10:02:41 +0100

    commit

--- route/index.js
+++ route/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const User = require('../models/users');
+const multer = require('multer');
+
 
 
 router.get('/', (req, res) => {
-    res.render('index', {title: 'Home-page'})
+    User.find().exec((err, users) => {
+        if (err) {
+            res.json({message: err.message})
+        } else {
+            res.render('index', {
+                title: 'Home-page',
+                users: users
+            })
+        }
+    })
 });
 
 
