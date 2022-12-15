7c2892deea7ad3fd0fc430d3ce20e1527ad7457b
alajido <96017433+Alajido@users.noreply.github.com>
2022-12-15 21:56:09 +0100

    commit

--- route/routes.js
+++ route/routes.js
@@ -42,7 +42,7 @@ router.post('/add-user', upload, (req, res) => {
     });
     user.save((err) => {
         if (err) {
-            res.json({message: err.message, type: 'bg-red-100'})
+            res.json({message: err.message, type: 'bg-red-100', border: 'border-red-500', text: 'text-red-700'})
         } else if (!req.file) {
             console.log('invalid file extension');
             return res.status(400).send({
@@ -64,18 +64,101 @@ router.post('/add-user', upload, (req, res) => {
         }
     })
 })
+// contact page
 router.get('/contact', (req, res) => {
     res.render('contact', {title: 'contact'})
 });
+// about page
 router.get('/about', (req, res) => {
     res.render('about', {title: 'about'})
 });
+// add user
 router.get('/add-user', (req, res) => {
     res.render('add-user', {title: 'add-user'})
 });
+
+// edit user
 router.get('/edit-user/:id', (req, res) => {
-    id
-    // res.render('edit-user', {title: 'edit-user'})
+
+    let id = req.params.id;
+    console.log(id, "hello")
+    User.findById(id, (err, user) => {
+        if (err) {
+            res.redirect('/')
+        } else {
+            if (user == null) {
+                res.redirect('/')
+            } else {
+                res.render('edit-user', {
+                    title: 'Edit user',
+                    user: user
+                })
+            }
+        }
+    })
 });
+
+// update a user
+router.post('/update/:id', upload, (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+
+    if (req.file) {
+        new_image =req.file.filename;;
+        try {
+            fs.unlinkSync('./uploads/' + req.body.old_image)
+        } catch(err) {
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+    User.findByIdAndUpdate(id, {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: new_image
+    }, (err, result) => {
+        if (err) {
+        res.json({message: err.message, type: 'bg-red-100', border: 'border-red-500', text: 'text-blue-700'})
+        } else {
+            req.session.message = {
+                type: 'bg-green-100',
+                border: 'border-blue-500',
+                text: 'text-blue-700',
+                message: 'user updated successfully'
+            }
+            res.redirect('/')
+        }
+    })
+})
+
+// Delete a user 
+router.get('/delete/:id', (req, res) => {
+    let id = req.params.id;
+
+    User.findOneAndRemove(id, (err, result) => {
+        if (result.image != '') {
+            try {
+                fs.unlinkSync('./uploads/' + result.image)
+            } catch(err) {
+                console.log(err)
+            }
+        };
+
+        if (err) {
+            res.json({message: err.message, type: 'bg-red-100', border: 'border-red-500', text: 'text-blue-700'})
+        } else {
+            req.session.message = {
+                type: 'bg-green-100',
+                border: 'border-blue-500',
+                text: 'text-blue-700',
+                message: 'user deleted successfully'
+            }
+            res.redirect('/')
+        }
+    })
+})
 
 module.exports = router;
